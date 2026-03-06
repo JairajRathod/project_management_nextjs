c8eb03f97bfa3b5869f99a744c5b88433f9b00d2
jayraj rathod <jarathod@bestpeers.in>
2026-03-06 12:48:11 +0530

    changes in next-auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,17 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-};
-
-const handler = NextAuth(authOptions);
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60, // 1 hour
+  },
+});
 
 export { handler as GET, handler as POST };
