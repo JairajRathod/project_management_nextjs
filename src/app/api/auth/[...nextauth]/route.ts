1886253b642c8ad59a02e0e9e586600f4a76661f
jayraj rathod <jarathod@bestpeers.in>
2026-03-06 14:26:38 +0530

    created db

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+import connectDB from "@/libs/connectDB";
+import { User } from "@/models/user.model";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -8,6 +10,31 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+
+  callbacks: {
+    async signIn({ user }) {
+      await connectDB();
+
+      const existingUser = await User.findOne({ email: user.email });
+
+      if (!existingUser) {
+        await User.create({
+          fullname: user.name,
+          email: user.email,
+          password: null, // because google login
+          role: "MEMBER",
+          avatar: user.image,
+          isVerified: true,
+          isActive: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+      }
+
+      return true;
+    },
+  },
+
   session: {
     strategy: "jwt",
     maxAge: 60 * 60, // 1 hour
---
+++ src/libs/connectDB.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongooseCache: MongooseCache | undefined;
+}
+
+const cached: MongooseCache = global.mongooseCache || {
+  conn: null,
+  promise: null,
+};
+
+global.mongooseCache = cached;
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "project_manager",
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
+
+export default connectDB;
