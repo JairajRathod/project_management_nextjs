bc25f0456f8032d4350c0f29a91721a27ecb1137
jayraj rathod <jarathod@bestpeers.in>
2026-03-09 19:34:59 +0530

    user service logic written

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,63 @@
-// create user
-export async function createUserService() {}
+import { User } from "@/models/user.model";
+import customError from "@/utils/customError";
 
-// read user
-export async function readUserService() {}
+// find the user details and send the response
+export async function findUserService(userId: string) {
+  try {
+    // checking user exist or not
+    const isUserExist = await User.findById(userId);
 
-// update user
-export async function updateUserService() {}
+    // throwing error if user not found
+    if (!isUserExist) {
+      throw new customError("User not Found", 404);
+    }
 
-// delete user
-export async function deleteUserService() {}
+    // return user details if user found
+    return isUserExist;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// find the user, update it and send response
+export async function findAndUpdateService(
+  userId: string,
+  updateDetails: object,
+) {
+  try {
+    // updating user details
+    const isUserUpdate = await User.findByIdAndUpdate(
+      { userId },
+      { $set: { ...updateDetails } },
+      { runValidators: true },
+    );
+
+    // throwing error if use details not updated
+    if (!isUserUpdate) {
+      throw new customError("User not updated", 400);
+    }
+
+    // return result if user updated
+    return isUserUpdate;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// find the user, delete it and send response
+export async function findAndDeleteService(userId: string) {
+  try {
+    // trying to delete the user
+    const isUserDeleted = await User.findByIdAndDelete(userId);
+
+    // throwing error if user not delete
+    if (!isUserDeleted) {
+      throw new customError("User not deleted", 400);
+    }
+
+    // return deleted user details
+    return isUserDeleted;
+  } catch (error) {
+    throw error;
+  }
+}
